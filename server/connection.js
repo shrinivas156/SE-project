7c4a2390f8f9ce9efcb18f6e38cfebb6fbb5cce3
shrinivas156 <91780372+shrinivas156@users.noreply.github.com>
2021-10-06 22:10:25 +0530

    Update connection.js

--- server/connection.js
+++ server/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
     host: "localhost",
     user:"root",
-    password:"2427",
+    password:"yourpassword",
     database:"Booking",
     multipleStatements:true
 });
